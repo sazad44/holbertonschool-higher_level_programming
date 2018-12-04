2f88730a39146a30dd1cf8a5019336146328b685
sazad44 <530@holbertonschool.com>
2018-12-04 20:47:05 +0000

    Uploading task 6 py script to print individual number combinations

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for x in range(10):
+    for y in range(10):
+        if y > x:
+            print("{}".format(x), end="")
+            print("{}".format(y), end="")
+            if x == 8 and y == 9:
+                print("")
+            else:
+                print("", end=", ")
