aec3af9a7c8e2f7fc41b3897ff1855a95b7895f8
sazad44 <530@holbertonschool.com>
2018-12-04 21:04:42 +0000

    Updating task 6 py script to fit print limits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,7 @@ for x in range(10):
     for y in range(10):
         if y > x:
             print("{}".format(x), end="")
-            print("{}".format(y), end="")
             if x == 8 and y == 9:
-                print("{:s}".format(""))
+                print("{}".format(y))
             else:
-                print("{:s}".format(""), end=", ")
+                print("{}".format(y), end=", ")
