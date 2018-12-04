683fc89253e71b94b9104813d0feb2edfd800312
sazad44 <530@holbertonschool.com>
2018-12-04 21:35:23 +0000

    Uploading task 8 py module to convert strings to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        char = ord(c)
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            char = ord(c) - ord('a') + ord('A')
+        print("{:c}".format(char), end="")
+    print("".format())
