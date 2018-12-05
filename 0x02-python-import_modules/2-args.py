445bd5d166209e0547e9a27b4f2e1ef9b538708b
sazad44 <530@holbertonschool.com>
2018-12-05 18:23:32 +0000

    Uploading task 2 py script to print command line args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+ac = len(sys.argv)
+if ac == 1:
+    print("{} arguments.".format(ac - 1))
+elif ac == 2:
+    print("{} argument:".format(ac - 1))
+    for x in range(1, ac):
+        print("{}: {}".format(x, sys.argv[x]))
+else:
+    print("{} arguments:".format(ac - 1))
+    for x in range(1, ac):
+        print("{}: {}".format(x, sys.argv[x]))
