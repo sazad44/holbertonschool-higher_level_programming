d43f4f0d94ec84a437390b95e8c1795bb6b8439a
sazad44 <530@holbertonschool.com>
2018-12-05 23:53:21 +0000

    Updating task 2 py script to not be executed on import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-ac = len(sys.argv)
-if ac == 1:
-    print("{} arguments.".format(ac - 1))
-elif ac == 2:
-    print("{} argument:".format(ac - 1))
-    for x in range(1, ac):
-        print("{}: {}".format(x, sys.argv[x]))
-else:
-    print("{} arguments:".format(ac - 1))
-    for x in range(1, ac):
-        print("{}: {}".format(x, sys.argv[x]))
+if __name__ == "__main__":
+    ac = len(sys.argv)
+    if ac == 1:
+        print("{} arguments.".format(ac - 1))
+    elif ac == 2:
+        print("{} argument:".format(ac - 1))
+        for x in range(1, ac):
+            print("{}: {}".format(x, sys.argv[x]))
+        else:
+            print("{} arguments:".format(ac - 1))
+            for x in range(1, ac):
+                print("{}: {}".format(x, sys.argv[x]))
