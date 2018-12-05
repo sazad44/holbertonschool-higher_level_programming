5d0235cb59a69b6b8f8c2650c9a1909eda9c4301
sazad44 <530@holbertonschool.com>
2018-12-05 01:54:53 +0000

    Updating task 13 c file to insert node when head is null

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,13 +14,18 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current, *temp;
 	listint_t *new;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
 	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 	for (current = *head; current; current = current->next)
 	{
 		if (number < current->next->n)
