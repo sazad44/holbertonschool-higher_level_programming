972c0cc3850116146a76a2c75fd892d9dbe5d21b
sazad44 <530@holbertonschool.com>
2018-12-05 20:33:42 +0000

    Updating task 0 py script to format for integers

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ import calculator_1 as op
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, op.add(a, b)))
-    print("{} - {} = {}".format(a, b, op.sub(a, b)))
-    print("{} * {} = {}".format(a, b, op.mul(a, b)))
-    print("{} / {} = {}".format(a, b, op.div(a, b)))
+    print("{:i} + {:i} = {:i}".format(a, b, op.add(a, b)))
+    print("{:i} - {:i} = {:i}".format(a, b, op.sub(a, b)))
+    print("{:i} * {:i} = {:i}".format(a, b, op.mul(a, b)))
+    print("{:i} / {:i} = {:i}".format(a, b, op.div(a, b)))
