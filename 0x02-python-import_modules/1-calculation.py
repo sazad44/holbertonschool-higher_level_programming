304e641c94bafd40b7d3d716fa7825f502bd15a8
sazad44 <530@holbertonschool.com>
2018-12-05 20:37:48 +0000

    Updating task 1 py script to use numer formatting

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ import calculator_1 as op
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{:i} + {:i} = {:i}".format(a, b, op.add(a, b)))
-    print("{:i} - {:i} = {:i}".format(a, b, op.sub(a, b)))
-    print("{:i} * {:i} = {:i}".format(a, b, op.mul(a, b)))
-    print("{:i} / {:i} = {:i}".format(a, b, op.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, op.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, op.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, op.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, op.div(a, b)))
