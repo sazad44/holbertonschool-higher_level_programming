865b8ded280010596575fef471c94565f80ef06e
sazad44 <530@holbertonschool.com>
2018-12-06 04:43:24 +0000

    Updating task 6 py script to not define variables if argv is too low

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,14 @@
 if __name__ == "__main__":
     import sys
     import calculator_1
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    o = sys.argv[2]
     if len(sys.argv) != 4:
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
         sys.exit(1)
-    elif o == '+':
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        o = sys.argv[2]
+    if o == '+':
         print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     elif o == '-':
         print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
