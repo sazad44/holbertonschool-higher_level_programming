4d433a31a326bfd81bfd53b448c4dbe24d067357
sazad44 <530@holbertonschool.com>
2018-12-06 04:30:28 +0000

    Updating task 6 py script to use variables and import all of calc mod

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 import sys
-from calculator_1 import add, sub, mul, div
+import calculator_1 as calc
 if __name__ == "__main__":
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    o = sys.argv[2]
     if len(sys.argv) != 4:
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
         sys.exit(1)
-    elif sys.argv[2] == '+':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3],
-                                    add(int(sys.argv[1]), int(sys.argv[3]))))
-    elif sys.argv[2] == '-':
-        print("{} - {} = {}".format(sys.argv[1], sys.argv[3],
-                                    sub(int(sys.argv[1]), int(sys.argv[3]))))
-    elif sys.argv[2] == '*':
-        print("{} * {} = {}".format(sys.argv[1], sys.argv[3],
-                                    mul(int(sys.argv[1]), int(sys.argv[3]))))
-    elif sys.argv[2] == '/':
-        print("{} / {} = {}".format(sys.argv[1], sys.argv[3],
-                                    div(int(sys.argv[1]), int(sys.argv[3]))))
+    elif o == '+':
+        print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    elif o == '-':
+        print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    elif o == '*':
+        print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    elif o == '/':
+        print("{} / {} = {}".format(a, b, calc.div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /".format())
         sys.exit(1)
