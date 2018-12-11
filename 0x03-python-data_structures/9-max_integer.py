3da53b5675013ced9173712aa5a039167663af92
sazad44 <530@holbertonschool.com>
2018-12-11 21:27:17 +0000

    Updating task 9 py module to check if data type is a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    try:
+    if isinstance(my_list, list) and not isinstance(my_list, str):
         if not len(my_list):
             return None
         max = 0
@@ -8,5 +8,5 @@ def max_integer(my_list=[]):
             if i > max:
                 max = i
         return max
-    except TypeError:
+    else:
         return None
