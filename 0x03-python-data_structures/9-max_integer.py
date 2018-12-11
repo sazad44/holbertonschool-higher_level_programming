131bed0e40fd66575b06c344be3f9645fc9c6604
sazad44 <530@holbertonschool.com>
2018-12-11 21:28:56 +0000

    Updating task 9 py module to handle lists of all negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if isinstance(my_list, list) and not isinstance(my_list, str):
         if not len(my_list):
             return None
-        max = 0
+        max = my_list[0]
         for i in my_list:
             if i > max:
                 max = i
