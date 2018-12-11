ad71142c438439a195bc14822a4124951ecdc721
sazad44 <530@holbertonschool.com>
2018-12-11 21:16:22 +0000

    Updating task 9 py module to reject non-lists correctly

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,10 +3,10 @@ def max_integer(my_list=[]):
     try:
         if not len(my_list):
             return None
-            max = 0
-            for i in my_list:
-                if i > max:
-                    max = i
-                    return max
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
     except TypeError:
         return
