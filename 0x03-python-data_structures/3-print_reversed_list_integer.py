5b89f3c9e36f6b3eb5404c70d05537bbeb0e8865
sazad44 <530@holbertonschool.com>
2018-12-11 20:58:36 +0000

    Updating task 2 py module to check for a list type

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:2d}".format(my_list[i]))
+    try:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
+    except TypeError:
+        return
 if __name__ == "__main__":
     pass
