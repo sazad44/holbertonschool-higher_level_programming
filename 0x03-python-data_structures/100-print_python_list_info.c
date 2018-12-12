45f68527abd941b3c439a9980d0c2e18d05901d0
sazad44 <530@holbertonschool.com>
2018-12-12 07:46:56 +0000

    Uploading task 14 c file to print python object data for further work

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <pyport.h>
+#include <object.h>
+#include <listobject.h>
+
+/**
+ * print_python_list_info - prints info about python lists
+ * @p: a pointer to a python object
+ * Return: No Value
+ */
+void print_python_list_info(PyObject *p)
+{
+	int i, psize, plsize;
+	char *ptype;
+
+	plsize = (int)PyList_Size(p);
+	psize = (int)Py_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", plsize);
+	printf("[*] Allocated = %d\n", psize);
+	for(i = 0; i <= plsize; i++)
+	{
+		ptype =  (char *)PyList_GetItem(p, i);
+		printf("Element %d: %s\n", i, ptype);
+	}
+}
