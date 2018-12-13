504d7bed4e4c648cda259f05f716523245d33137
sazad44 <530@holbertonschool.com>
2018-12-13 18:55:21 +0000

    Uploading task 9 py module to create a new dictionary with altered values

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if isinstance(a_dictionary, dict):
+        new_dict = {k: a_dictionary[k] * 2 for k in a_dictionary}
+        return new_dict
