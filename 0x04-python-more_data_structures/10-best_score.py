db4eb5f1c73ab3ba870ccb2aecabb4a1a918db73
sazad44 <530@holbertonschool.com>
2018-12-13 21:22:28 +0000

    Updating task 10 py module to deal with empty dictionaries

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
+    if isinstance(a_dictionary, dict) and len(a_dictionary):
         comp = max([v for v in a_dictionary.values()])
         for k in a_dictionary.keys():
             if a_dictionary[k] == comp:
                 return k
-        return None
