930f7a6ed342a6df72ede05ea8209e66c4df3a3c
sazad44 <530@holbertonschool.com>
2018-12-14 03:26:55 +0000

    Updating task 12 py module to handle roman subtraction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,9 @@ def roman_to_int(roman_string):
                      'M': 1000}
         minsum = retsum = curr_v = count = 0
         for c in roman_string:
-            if count == 2:
-                minsum = count = 0
             rom_v = roman_ref[c]
+            if rom_v <= curr_v:
+                minsum = 0
             if rom_v > curr_v:
                 rom_v -= minsum
                 retsum -= minsum
@@ -15,7 +15,6 @@ def roman_to_int(roman_string):
             retsum += rom_v
             minsum += rom_v
             curr_v = rom_v
-            count += 1
         return retsum
     else:
         return 0
