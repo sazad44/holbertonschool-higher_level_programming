a727c2567759a17beb889e0b2415a97a43aac0e4
sazad44 <530@holbertonschool.com>
2018-12-14 03:49:19 +0000

    Updating task 13 py module to return 0 with empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if isinstance(my_list, list):
+    if isinstance(my_list, list) and len(my_list):
         numer = sum([e[0] * e[1] for e in my_list])
         denom = sum([e[1] for e in my_list])
         return (numer/denom)
