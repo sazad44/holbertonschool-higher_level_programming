4144dd96e837f0cad1950f2ef4e13d4a0c49a4fe
sazad44 <530@holbertonschool.com>
2018-12-14 03:48:07 +0000

    Uploading task 13 py module to ascertain the weighted average of a list of tuples

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if isinstance(my_list, list):
+        numer = sum([e[0] * e[1] for e in my_list])
+        denom = sum([e[1] for e in my_list])
+        return (numer/denom)
+    return 0
