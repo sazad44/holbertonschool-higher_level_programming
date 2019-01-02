fdda871510b38e231082a0741bd528bdc846d045
sazad44 <530@holbertonschool.com>
2019-01-02 20:21:57 +0000

    Updating task 7 py file to handle type error of input

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as V:
+    except (TypeError, ValueError) as V:
         sys.stderr.write("Exception: " + V.args[0] + "\n")
         return False
