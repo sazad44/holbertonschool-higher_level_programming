4e132ba52ca1a19478d6f8be4366db3fa4649e7e
sazad44 <530@holbertonschool.com>
2019-01-03 20:11:11 +0000

    Uploading task 1 py file to declare square class with private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    __size = 0
+
+    def __init__(self, size):
+        """__init__ method to initialize class
+
+        Args:
+        self: the object itself
+        size (int): the size of the square
+
+        Returns: No Value
+        """
+        self.__size = size
