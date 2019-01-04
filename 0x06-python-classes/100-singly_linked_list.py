80f755d8723c5d171614cc4ef558a9ed789630e3
sazad44 <530@holbertonschool.com>
2019-01-04 07:32:49 +0000

    Updating task 7 py file to define classes for further work

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 class Node:
     def __init__(self, data, next_node=None):
-        pass
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
@@ -12,3 +13,29 @@ class Node:
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @data.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        if self.__head is None:
+            self.__head = Node(value)
+        elif value < self.__head.data:
+            tmp = Node(value, self.__head)
+            self.__head = tmp
+
+    def __str__(self):
+        return "{:d}".format(self.__head.data)
