e072ff5911b69bface10f489dc4b1405253ccfdf
sazad44 <530@holbertonschool.com>
2019-01-08 22:03:23 +0000

    Uploading task 5 py file and test py file for max integer finding and testing

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """TestMaxInteger class is a unit testing class for the function max_integer
+    """
+
+    def test_max(self):
+        """test_max method
+        tests a normal list and if the function gets the max
+        """
+        self.assertEqual(max_integer([4, 6, 3, 5, 78, 4, 3, 4, 1, 9]), 78)
+
+    def test_empty(self):
+        """test_empty method
+        tests function with empty list
+        """
+        self.assertEqual(max_integer([]), None)
+
+    def test_equal(self):
+        """test_equal method
+        tests function with list with duplicates
+        """
+        self.assertEqual(max_integer([3, 3, 3]), 3)
+
+    def test_error(self):
+        """test_error method
+        tests function in error situation
+        """
+        with self.assertRaises(TypeError):
+            max_integer(['a', 3, 4])
+
+    def test_none(self):
+        """test_none method
+        tests function when none is passed
+        """
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_negative(self):
+        """test_negative method
+        tests function if parts of the list are negative
+        """
+        self.assertEqual(max_integer([3, -4, 5, -6]), 5)
+
+    def test_one_input(self):
+        """test_one_input method
+        tests function with one element in the list
+        """
+        self.assertEqual(max_integer([4]), 4)
+
+    def test_ex1(self):
+        """test_ex1 method
+        tests function with first example
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_ex2(self):
+        """test_ex2 method
+        tests function with first example
+        """
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
