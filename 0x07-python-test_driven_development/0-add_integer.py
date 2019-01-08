b33a83e57d09ebe8b9e164a4ec62673a3df529f8
sazad44 <530@holbertonschool.com>
2019-01-08 00:31:08 +0000

    Uploading task 0 py file to add two integers and corresponding tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Add Integer Module
+
+This module adds two integers and rounds floats, checking types as well.
+
+"""
+
+def add_integer(a, b=98):
+    """add_integer function to add a and b
+    Return: Result of a and b addition
+    """
+    try:
+        if isinstance(a, float):
+            a = int(round(a))
+        if isinstance(b, float):
+            b = int(round(a))
+        return a + b
+    except TypeError:
+        if isinstance(a, int):
+            raise TypeError('b must be an integer')
+        elif isinstance(b, int):
+            raise TypeError('a must be an integer')
---
+++ 0x07-python-test_driven_development/tests/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from integer import add_integer
+print(add_integer(1,2))
