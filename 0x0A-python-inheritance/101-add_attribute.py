6d5a750166692d7b7505bd551abbf6007cfc5e05
sazad44 <530@holbertonschool.com>
2019-01-17 07:23:25 +0000

    Uploading task 13 py file to assign attribute to object if possible

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""101-add_attribute module"""
+def add_attribute(obj, key, value):
+    """add_attribute function"""
+    if not isinstance(key, (str, int, tuple, float, bool)) or type(obj) == str:
+        raise TypeError("can't add new attribute")
+    else:
+        obj.__dict__[key] = value
