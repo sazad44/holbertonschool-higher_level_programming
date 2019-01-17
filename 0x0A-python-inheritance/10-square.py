879dc9cf4fd06d781fdcc2989ba0d13972a9526e
sazad44 <530@holbertonschool.com>
2019-01-17 18:24:42 +0000

    Updating task 10 with reviews of errors and inheritance correctly

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,39 +2,7 @@
 """10-square module"""
 
 
-class BaseGeometry(object):
-    """BaseGeometry Class"""
-
-    def area(self):
-        """area method raises exception"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """int validator method validates value as integer"""
-        if isinstance(name, str):
-            if not isinstance(value, int):
-                raise TypeError("{} must be an integer".format(str(name)))
-            elif value <= 0:
-                raise ValueError("{} must be greater than 0".format(str(name)))
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle Class"""
-
-    def __init__(self, width, height):
-        """init magic method initialization procedure"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """area method calculates and returns area of rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """str magic method"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -42,5 +10,5 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """__init__ magic method"""
-        super().__init__(size, size)
+        super().integer_validator("size", size)
         self.__size = size
