7c6237fd4eb9cbb03e965e93c73610ecc55bd969
sazad44 <530@holbertonschool.com>
2019-01-17 18:44:10 +0000

    Updating task 13 py file to not use try/except

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,9 @@
 
 def add_attribute(obj, key, value):
     """add_attribute function"""
-    try:
-        obj.__dict__[key] = value
-    except:
+    if not isinstance(key, (int, str, tuple, float, bool, complex, range)):
         raise TypeError("can't add new attribute")
+    elif not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    else:
+        obj.__dict__[key] = value
