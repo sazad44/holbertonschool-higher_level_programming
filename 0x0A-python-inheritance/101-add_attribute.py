b5e2180dc08fb6b21b910d3a0225b9ff1da0b073
sazad44 <530@holbertonschool.com>
2019-01-17 18:37:27 +0000

    Updating task 13 py file with correct error handling

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(obj, key, value):
     """add_attribute function"""
-    if not isinstance(key, (str, int, tuple, float, bool)) or type(obj) == str:
-        raise TypeError("can't add new attribute")
-    else:
+    try:
         obj.__dict__[key] = value
+    except:
+        raise TypeError("can't add new attribute")
