afee2e230ad7174c3ad1d4fae1fd14adc54835d3
sazad44 <530@holbertonschool.com>
2019-01-17 22:07:03 +0000

    Uploading task 9 to script a list appending process

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+items = []
+
+if len(sys.argv) == 1:
+    save_to_json_file(items, "add_item.json")
+else:
+    items = load_from_json_file("add_item.json")
+    items.extend([sys.argv[i] for i in range(1, len(sys.argv))])
+    save_to_json_file(items, "add_item.json")
