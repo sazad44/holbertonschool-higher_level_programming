dcf0a88b8ebc8cc1e9555b608a51b95d9fbe43e5
sazad44 <530@holbertonschool.com>
2019-01-18 06:29:53 +0000

    Updating task 2 py file to handle problematic inputs for read_lines function

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -4,10 +4,14 @@
 
 def read_lines(filename="", nb_lines=0):
     """read_lines function"""
-    with open(filename) as f:
-        if nb_lines <= 0 or nb_lines >= len(f.readlines()):
-            print(f.read(), end="")
-            return
-    with open(filename) as f:
-        for line in range(nb_lines):
-            print(f.readline(), end="")
+    if type(filename) == str:
+        with open(filename) as f:
+            if type(nb_lines) != int:
+                print(f.read(), end="")
+                return
+            elif nb_lines <= 0 or nb_lines >= len(f.readlines()):
+                print(f.read(), end="")
+                return
+        with open(filename) as f:
+            for line in range(nb_lines):
+                print(f.readline(), end="")
