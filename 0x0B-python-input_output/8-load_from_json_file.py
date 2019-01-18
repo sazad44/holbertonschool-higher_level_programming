aa957500be0fd3267b9dce5ff7b9cc65bd3be78c
sazad44 <530@holbertonschool.com>
2019-01-18 06:45:57 +0000

    Updating task 8 py file to only open file for reading

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -6,5 +6,5 @@ def load_from_json_file(filename):
     """load_from_json_file function"""
     import json
 
-    with open(filename, "r+") as f:
+    with open(filename, "r") as f:
         return json.loads(f.read())
