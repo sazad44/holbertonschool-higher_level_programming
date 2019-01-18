e7d8a6f2d6345c1b4036aa62bb15fde78703e294
sazad44 <530@holbertonschool.com>
2019-01-18 06:43:36 +0000

    Updating task 2 py file to reset offset after line check

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -9,7 +9,9 @@ def read_lines(filename="", nb_lines=0):
             if type(nb_lines) != int:
                 print(f.read(), end="")
                 return
-            elif nb_lines <= 0 or nb_lines >= len(f.readlines()):
+        with open(filename) as f:
+            if nb_lines <= 0 or nb_lines >= len(f.readlines()):
+                f.seek(0)
                 print(f.read(), end="")
                 return
         with open(filename) as f:
