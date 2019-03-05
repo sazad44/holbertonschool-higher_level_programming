80d98ff38dedee87e30f40d782656bac2711627d
sazad44 <530@holbertonschool.com>
2019-03-05 17:05:07 +0000

    Uploading task 0 sql script to show all privileges for two users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges of users
+-- Lists all privileges for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Lists all privileges for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
