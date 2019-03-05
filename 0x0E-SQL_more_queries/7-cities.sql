5fc97537e53a241c33b4fd8ae42ce01d5ba200be
sazad44 <530@holbertonschool.com>
2019-03-05 20:38:05 +0000

    Updating task 7 sql script with ordering of property modifications

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,12 @@
 -- Creates dtabase and table according to specs
 -- Create database hbtn_0d_usa if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Create table citieswith id, state_id, name
+-- Create table cities with id, state_id, name
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id),
        FOREIGN KEY (state_id)
-       REFERENCES `states`(`id`),
-       name VARCHAR(256) NOT NULL
+       REFERENCES `states`(`id`)
 );
