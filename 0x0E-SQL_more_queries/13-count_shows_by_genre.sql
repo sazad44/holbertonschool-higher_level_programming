83377ac04da021f0362b4c0b90209ec434c466f5
sazad44 <530@holbertonschool.com>
2019-03-05 22:25:12 +0000

    Uploading task 13 sql script to count number of shows in each genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Lists all genres from database and provides count of matching entries
+-- Lists all genres in tv shows database and counts shows matching genres
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
+       FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+       GROUP BY tv_genres.name ORDER BY `number_of_shows` DESC;
