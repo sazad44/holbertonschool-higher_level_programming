779d70eefe49e512bd5d9439789d04abf9c98b5b
sazad44 <530@holbertonschool.com>
2019-03-05 02:42:02 +0000

    Uploading task 11 sql script to list records with a score greater than or equal to 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records of a table in a certain order
+-- Lists all records by score and name orderedy by score
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
