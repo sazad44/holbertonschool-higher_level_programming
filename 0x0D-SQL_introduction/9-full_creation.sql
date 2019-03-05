2f555644f5b1f44ec2d68a76fa938c95625d26c4
sazad44 <530@holbertonschool.com>
2019-03-05 02:28:28 +0000

    Updating task 9 sql script to skip table creation if table already exists

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Script creates table and adds multiple rows
 -- Create New Table with id INT name V256 score INT
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 -- INSERT first record
 INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, "John", 10);
 -- INSERT second record
