beed186db784c288637bec2012ca0dac27eb13d4
sazad44 <530@holbertonschool.com>
2019-03-05 22:51:37 +0000

    Updating task 14 sql script to use hardcoded id

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,3 @@
 -- Lists all genres of a show
 -- Lists all genres of the show Dexter
-SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.show_id = (SELECT `id` FROM tv_shows WHERE tv_shows.title = 'Dexter');
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.show_id = 8;
