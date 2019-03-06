c70c0b2f0af326893bac47322e7f3e2e66a3df07
sazad44 <530@holbertonschool.com>
2019-03-06 04:18:20 +0000

    Updating task 16 sql script to change order of query actions and include ASC

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List all shows and genres linked to that show
 -- List all shows an their genres
-SELECT tv_shows.title, tv_genres.name FROM tv_show_genres
-       RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+       LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
        LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-       GROUP BY tv_shows.title, tv_genres.name ORDER BY tv_shows.title, tv_genres.name;
+       ORDER BY tv_shows.title, tv_genres.name ASC;
