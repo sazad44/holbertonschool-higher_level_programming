8571550d6a9ac85ba020aa95408e147e75ad8bbb
sazad44 <530@holbertonschool.com>
2019-03-28 21:37:37 +0000

    Uploading task 2 py script to find certain states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Select States Script to extract states from database table"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    states = db.cursor()
+    states.execute("SELECT * FROM states WHERE `name` = '{}' ORDER BY 'states.id' ASC".format(sys.argv[4]))
+    for state in states:
+            print ("({}, '{}')".format(state[0], state[1]))
+    states.close()
+    db.close()
