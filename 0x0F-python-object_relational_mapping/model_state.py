eaa6bf8d19532302fa4bf6396802c193effbfd4f
sazad44 <530@holbertonschool.com>
2019-03-29 05:17:23 +0000

    Updating task 6 py file to define Base instance and class State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 """Module holding class definition of state and instance of declarative_base"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                unique=True,
+                nullable=False)
+    name = Column(String(128),
+                  nullable=False)
