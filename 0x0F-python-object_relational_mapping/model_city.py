98c8d1891dede7e9f06c2408409f570d28698b0b
sazad44 <530@holbertonschool.com>
2019-03-30 05:30:24 +0000

    Updating task 14 city class to be only relationship holder

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -14,4 +14,4 @@ class City(Base):
                 primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
+    state = relationship("State", backref="state_id")
