ebf79dbe873d054c663ffdb684e04a7e2509c241
sazad44 <530@holbertonschool.com>
2019-03-30 20:44:12 +0000

    Uploading task 16 py script to print states and their corresponding cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Py script to list all objects and their corresponding children"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for sta in session.query(State):
+        print("{}: {}".format(sta.id, sta.name))
+        for cit in sta.cities:
+            print("\t{}: {}".format(cit.id, cit.name))
+    session.close()
+    engine.dispose()
