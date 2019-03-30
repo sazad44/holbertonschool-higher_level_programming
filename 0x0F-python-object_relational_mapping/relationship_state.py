dd09e202643857aa38fdf5c4510f35c922663fc3
sazad44 <530@holbertonschool.com>
2019-03-30 18:31:16 +0000

    Updating task 15 py script and classes with correct relationship definitions and class definitions

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -18,4 +18,3 @@ class State(Base):
                 nullable=False)
     name = Column(String(128),
                   nullable=False)
-    cities = relationship("City", cascade="all,delete", backref="state")
