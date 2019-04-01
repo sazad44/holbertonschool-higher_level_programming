0e0d820ba48f4941365e5a01c995a445e31ae5c5
sazad44 <530@holbertonschool.com>
2019-04-01 00:48:42 +0000

    Updating task 16 py script to use cities attribute more directly

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for sta in session.query(State).order_by(State.id):
+    for sta in session.query(State)\
+                      .join(State.cities)\
+                      .order_by(State.id):
         print("{}: {}".format(sta.id, sta.name))
         for cit in sta.cities:
             print("    {}: {}".format(cit.id, cit.name))
