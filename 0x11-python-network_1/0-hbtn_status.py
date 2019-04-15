d93e1144ec9ea9c9f016ea55e3f25dfc6e59e611
sazad44 <530@holbertonschool.com>
2019-04-15 19:42:02 +0000

    Updating to use tabs with task 0 py file to get request content"

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as u:
         print('Body response:')
         response = u.read()
-        print('    - type: {}'.format(type(response)))
-        print('    - content: {}'.format(response))
-        print('    - utf8 content: {}'.format(response.decode('utf-8')))
+        print('\t- type: {}'.format(type(response)))
+        print('\t- content: {}'.format(response))
+        print('\t- utf8 content: {}'.format(response.decode('utf-8')))
