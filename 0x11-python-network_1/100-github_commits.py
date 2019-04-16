127da5c9de7e29ab7ab46317737e1bf5d1be2131
sazad44 <530@holbertonschool.com>
2019-04-16 01:02:54 +0000

    Uploading task 11 py script to check ability to sort

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Py script to list 10 commits of a repo"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    resDict = {}
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    response = requests.get(url).json()
+    for i in response:
+        sha = i.get('sha')
+        name = i.get('commit').get('author').get('name')
+        resDict[i.get('commit').get('author').get('date')] = '{}: {}'.format(sha, name)
+    for i in range(10):
+        print(resDict[sorted(resDict.keys())[i]])
