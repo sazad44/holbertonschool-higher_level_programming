238e21e40de0c434c9a2af063c2c3d321601e470
sazad44 <530@holbertonschool.com>
2019-04-16 01:09:24 +0000

    Updating task 11 py script to grab 10 most recent commits to repo

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,9 +7,7 @@ if __name__ == "__main__":
     resDict = {}
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
     response = requests.get(url).json()
-    for i in response:
-        sha = i.get('sha')
-        name = i.get('commit').get('author').get('name')
-        resDict[i.get('commit').get('author').get('date')] = '{}: {}'.format(sha, name)
     for i in range(10):
-        print(resDict[sorted(resDict.keys())[i]])
+        sha = response[i].get('sha')
+        name = response[i].get('commit').get('author').get('name')
+        print("{}: {}".format(sha, name))
