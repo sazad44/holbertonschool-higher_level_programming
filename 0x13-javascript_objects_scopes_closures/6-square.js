7383093d95b9c74e2cad57ac4e2e5b4e4e040523
sazad44 <530@holbertonschool.com>
2019-05-13 22:49:50 +0000

    Updating task 6 js file to inherit from task 5 Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,68 +1,17 @@
 #!/usr/bin/node
+const Square = require('./5-square');
 
-class Rectangle {
-  constructor (w, h) {
-    if ((w > 0 && typeof w !== 'undefined') && (h > 0 && typeof h !== 'undefined')) {
-      this.width = w;
-      this.height = h;
+Square.prototype.charPrint = function (c) {
+  if (typeof c === 'undefined') {
+    c = 'X';
+  }
+  for (let h = 0; h < this.height; h++) {
+    let array = [];
+    for (let w = 0; w < this.width; w++) {
+      array.push(c);
     }
-  }
-  // Prints the rectangle based on dimensions
-  print () {
-    for (let h = 0; h < this.height; h++) {
-      let array = [];
-      for (let w = 0; w < this.width; w++) {
-        array.push('X');
-      }
-      console.log(array.join(''));
-    }
-  }
-  // Swaps the values of height and width
-  rotate () {
-    let temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-  // Doubles dimensions of rectangle
-  double () {
-    this.width *= 2;
-    this.height *= 2;
+    console.log(array.join(''));
   }
 }
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.charPrint = function (c) {
-      if (typeof c === 'undefined') {
-        c = 'X';
-      }
-      for (let h = 0; h < this.height; h++) {
-        let array = [];
-        for (let w = 0; w < this.width; w++) {
-          array.push(c);
-        }
-        console.log(array.join(''));
-      }
-    };
-  }
-}
-
-/* Prints square with specified character
-  charPrint (c) {
-    if (typeof c === 'undefined') {
-      c = 'X';
-    }
-    for (let h = 0; h < this.height; h++) {
-      let array = [];
-      for (let w = 0; w < this.width; w++) {
-        array.push(c);
-      }
-      console.log(array.join(''));
-    }
-  }
-}
-*/
-
-module.exports = Rectangle;
 module.exports = Square;
