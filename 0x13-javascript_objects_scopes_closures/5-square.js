6f7fe7156349d36562e75b74528f67dd034a8e2e
sazad44 <530@holbertonschool.com>
2019-05-13 21:57:12 +0000

    Uploading task 5 js file to create inherited class from rectangle called square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0 && typeof w !== 'undefined') && (h > 0 && typeof h !== 'undefined')) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  // Prints the rectangle based on dimensions
+  print () {
+    for (let h = 0; h < this.height; h++) {
+      let array = [];
+      for (let w = 0; w < this.width; w++) {
+        array.push('X');
+      }
+      console.log(array.join(''));
+    }
+  }
+  // Swaps the values of height and width
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  // Doubles dimensions of rectangle
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
