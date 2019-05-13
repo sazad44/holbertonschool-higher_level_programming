8edd8cd437a5fadc03a70fa6fb549a96b0e372b5
sazad44 <530@holbertonschool.com>
2019-05-13 21:00:54 +0000

    Updating task 3 js file with semi style compliance"

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0 && typeof w !== 'undefined') && (h > 0 && typeof h !== 'undefined')) {
+      this.width = w;
+      this.height = h;
+    }
+    this.print = function () {
+      for (let h = 0; h < this.height; h++) {
+        let array = [];
+        for (let w = 0; w < this.width; w++) {
+          array.push('X');
+        }
+        console.log(array.join(''));
+      }
+    };
+  }
+}
+
+module.exports = Rectangle;
