9c0c4928a3227599f8a1e9845af3380bc07b62b5
sazad44 <530@holbertonschool.com>
2019-05-14 01:36:32 +0000

    Updating task 13 js file to be semistandard compliant and not use var

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,34 +1,32 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.exists(process.argv[2], function(exists) {
-  if (exists) {
-    fs.stat(process.argv[2], function(error, stats) {
-      let fd = fs.open(process.argv[2], 'r', function(error, fd) {
-	var buffer = new Buffer.alloc(stats.size);
-	fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-	  var data = buffer.toString('utf-8', 0, buffer.length);
-	  fs.writeFile(process.argv[4], data, function (err) {
-	    if (err) throw err;
-	  })
-	})
-      })
-    })
-  }
-})
+fs.stat(process.argv[2], function (error, stats) {
+  if (error) throw error;
+  fs.open(process.argv[2], 'r', function (error, fd) {
+    if (error) throw error;
+    let buffer = new Buffer.Alloc(stats.size);
+    fs.read(fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+      if (error) throw error;
+      let data = buffer.toString('utf-8', 0, buffer.length);
+      fs.writeFile(process.argv[4], data, function (err) {
+        if (err) throw err;
+      });
+    });
+  });
+});
 
-fs.exists(process.argv[3], function(exists) {
-  if (exists) {
-    fs.stat(process.argv[3], function(error, stats) {
-      let fd = fs.open(process.argv[3], 'r', function(error, fd) {
-	var buffer = new Buffer.alloc(stats.size);
-	fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-	  var data = buffer.toString('utf-8', 0, buffer.length);
-	  fs.appendFile(process.argv[4], data, function (err) {
-	    if (err) throw err;
-	  })
-	})
-      })
-    })
-  }
-})
+fs.stat(process.argv[3], function (error, stats) {
+  if (error) throw error;
+  fs.open(process.argv[3], 'r', function (error, fd) {
+    if (error) throw error;
+    let buffer = new Buffer.Alloc(stats.size);
+    fs.read(fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+      if (error) throw error;
+      let data = buffer.toString('utf-8', 0, buffer.length);
+      fs.appendFile(process.argv[4], data, function (err) {
+        if (err) throw err;
+      });
+    });
+  });
+});
