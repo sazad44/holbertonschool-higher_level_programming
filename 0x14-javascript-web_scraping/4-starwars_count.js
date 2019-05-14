64765971ecb3104430901e41f366da41608f33ed
sazad44 <530@holbertonschool.com>
2019-05-14 21:19:55 +0000

    Updating task 4 js file to return error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,12 +8,13 @@ request(process.argv[2], function (error, response, body) {
       console.log(error);
     } else {
       for (let i = 0; i < JSON.parse(body)['results'].length; i++) {
-        if (JSON.parse(body)['results'][i]['characters'].indexOf('https://swapi.co/api/people/18/') !== -1) {
+        if (JSON.parse(body)['results'][i]['characters'].indexOf('https://swapi.co/api/people/18/') >= 0) {
           count += 1;
         }
       }
       console.log(count);
     }
   } catch (e) {
+    console.log(e);
   }
 });
