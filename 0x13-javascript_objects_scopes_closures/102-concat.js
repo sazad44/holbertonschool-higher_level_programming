7f05d39214ff4db9951dfae07066eab9c2b8a689
sazad44 <530@holbertonschool.com>
2019-05-14 02:01:17 +0000

    Updating task 13 js file to be semistandard compliant and to use size of file as reading stat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,9 +5,9 @@ fs.stat(process.argv[2], function (error, stats) {
   if (error) throw error;
   fs.open(process.argv[2], 'r', function (error, fd) {
     if (error) throw error;
-    fs.readFile(process.argv[2], function read(err, data) {
-      if (error) throw error;
-      let fa = data.toString('utf-8', 0, 100);
+    fs.readFile(process.argv[2], function read (err, data) {
+      if (err) throw err;
+      let fa = data.toString('utf-8', 0, stats.size);
       fs.writeFile(process.argv[4], fa, function (err) {
         if (err) throw err;
       });
@@ -19,9 +19,9 @@ fs.stat(process.argv[3], function (error, stats) {
   if (error) throw error;
   fs.open(process.argv[3], 'r', function (error, fd) {
     if (error) throw error;
-    fs.readFile(process.argv[3], function read(err, data) {
-      if (error) throw error;
-      let fa = data.toString('utf-8', 0, 100);
+    fs.readFile(process.argv[3], function read (err, data) {
+      if (err) throw err;
+      let fa = data.toString('utf-8', 0, stats.size);
       fs.appendFile(process.argv[4], fa, function (err) {
         if (err) throw err;
       });
