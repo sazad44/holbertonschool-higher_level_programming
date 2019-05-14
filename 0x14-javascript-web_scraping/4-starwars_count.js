2f500586fd8c59b34083b6f57f292d08be84e22c
sazad44 <530@holbertonschool.com>
2019-05-14 21:44:15 +0000

    Updating task 4 js file with semistandard compliance

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,11 +10,11 @@ request(process.argv[2], function (error, response, body) {
       let results = JSON.parse(body)['results'];
       for (let i = 0; i < results.length; i++) {
         for (let c = 0; c < results[i]['characters'].length; c++) {
-	  for (let ch = 0; ch < results[i]['characters'][c].length; ch++) {
-	    if (results[i]['characters'][c].charAt(ch) === '1' && results[i]['characters'][c].charAt(ch + 1) === '8') {
+          for (let ch = 0; ch < results[i]['characters'][c].length; ch++) {
+            if (results[i]['characters'][c].charAt(ch) === '1' && results[i]['characters'][c].charAt(ch + 1) === '8') {
               count += 1;
-	    }
-	  }
+            }
+          }
         }
       }
       console.log(count);
