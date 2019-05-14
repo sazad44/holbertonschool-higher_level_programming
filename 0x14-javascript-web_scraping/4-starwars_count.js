ad48362440f9551acadb5f9ff4b8bdc881398cd0
sazad44 <530@holbertonschool.com>
2019-05-14 21:39:22 +0000

    Updating task 4 js file to find any instance of 18 in the mentioned results

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,9 +7,14 @@ request(process.argv[2], function (error, response, body) {
     if (error) {
       console.log(error);
     } else {
-      for (let i = 0; i < JSON.parse(body)['results'].length; i++) {
-        if (JSON.parse(body)['results'][i]['characters'].indexOf('https://swapi.co/api/people/18/') >= 0) {
-          count += 1;
+      let results = JSON.parse(body)['results'];
+      for (let i = 0; i < results.length; i++) {
+        for (let c = 0; c < results[i]['characters'].length; c++) {
+	  for (let ch = 0; ch < results[i]['characters'][c].length; ch++) {
+	    if (results[i]['characters'][c].charAt(ch) === '1' && results[i]['characters'][c].charAt(ch + 1) === '8') {
+              count += 1;
+	    }
+	  }
         }
       }
       console.log(count);
