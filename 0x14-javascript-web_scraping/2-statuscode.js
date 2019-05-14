500c0b33994189a4944918ecafbed611086eadc4
sazad44 <530@holbertonschool.com>
2019-05-14 20:23:55 +0000

    Updating task 2 js file with semistandard compliance

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const response = request.get(process.argv[2], function(error, response) {
+request.get(process.argv[2], function (error, response) {
   if (error) {
     console.log(error);
   } else {
