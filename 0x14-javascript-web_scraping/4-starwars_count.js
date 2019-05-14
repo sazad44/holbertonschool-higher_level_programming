d2f794fd83ad3952c18386bf5c8e8e17ec120e43
sazad44 <530@holbertonschool.com>
2019-05-14 21:12:37 +0000

    Updating task 4 js file to handle json parsing error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], function(error, response, body) {
-  let count = 0;
-  if (error) {
-    console.log(error);
-  } else {
-    for (let i = 0; i < JSON.parse(body)['results'].length; i++) {
-      if (JSON.parse(body)['results'][i]['characters'].indexOf('https://swapi.co/api/people/18/') !== -1) {
-	count += 1;
+request(process.argv[2], function (error, response, body) {
+  try {
+    let count = 0;
+    if (error) {
+      console.log(error);
+    } else {
+      for (let i = 0; i < JSON.parse(body)['results'].length; i++) {
+        if (JSON.parse(body)['results'][i]['characters'].indexOf('https://swapi.co/api/people/18/') !== -1) {
+          count += 1;
+        }
       }
+      console.log(count);
     }
-    console.log(count);
+  } catch (e) {
+    console.log(0);
   }
 });
