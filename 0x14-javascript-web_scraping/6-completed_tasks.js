32937213c8ccc048f7cd36f4ed6e35e8b3c9f03c
sazad44 <530@holbertonschool.com>
2019-05-14 22:48:46 +0000

    Updating task 6 js file to only assign a count for a user if they have one completed task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,7 +6,7 @@ request(process.argv[2], function (error, response, body) {
   let newDict = {};
   let todoList = JSON.parse(body);
   Object.values(todoList).map(x => {
-    if (typeof newDict[x['userId']] === 'undefined') newDict[x['userId']] = 0;
+    if (typeof newDict[x['userId']] === 'undefined' && x['completed'] === true) newDict[x['userId']] = 1;
     if (x['completed'] === true) newDict[x['userId']] += 1;
   });
   console.log(newDict);
