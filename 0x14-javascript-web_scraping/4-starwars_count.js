116232d7d04cce26d963db8a9dd9f8de88100910
sazad44 <530@holbertonschool.com>
2019-05-14 21:03:06 +0000

    Uploading task 4 js file to count number of films Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function(error, response, body) {
+  let count = 0;
+  if (error) {
+    console.log(error);
+  } else {
+    for (let i = 0; i < JSON.parse(body)['results'].length; i++) {
+      if (JSON.parse(body)['results'][i]['characters'].indexOf('https://swapi.co/api/people/18/') !== -1) {
+	count += 1;
+      }
+    }
+    console.log(count);
+  }
+});
