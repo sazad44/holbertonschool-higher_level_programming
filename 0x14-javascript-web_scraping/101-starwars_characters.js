902131fabdd72a03bc739e3f4fdcf9075376f833
sazad44 <530@holbertonschool.com>
2019-05-15 15:30:21 +0000

    Uploading task 8 js file to print characters of a movie in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,36 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 
-const retDict = {};
-
-function repAgain (idx, length, url) {
-  if (idx >= length) {
-  } else {
-    request(url[idx], function (error, response, body) {
-      console.log(JSON.parse(body)['name']);
-    });
-  }
+function reqAgain (i, characters, retDict) {
+  if (i === characters.length) return retDict;
+  request(characters[i], function (error, response, body) {
+    if (error) console.log(error);
+    console.log(JSON.parse(body)['name']);
+    reqAgain(i + 1, characters, retDict);
+  });
 }
 
-function reqAgain (idx, length, url, repAgain) {
-  if (idx >= length) {
-  } else {
-    console.log('WOWOWOWOW' + url[idx]);
-    console.log('WOWOWOWOW' + url[idx + 1]);
-    request(url[idx], function (error, response, body) {
-      console.log(JSON.parse(body)['name']);
-    });
-    repAgain();
-  }
-}
-
-function reqResolution(filmNo) {
+function reqResolution (filmNo) {
   request('http://swapi.co/api/films/' + filmNo, function (error, response, body) {
     if (error) console.log(error);
     let characters = JSON.parse(body)['characters'];
-    for (let i = 0; i < characters.length; i++) {
-      reqAgain(i, characters.length, characters, reqAgain (i + 1, characters.length, characters));
-    };
+    reqAgain(0, characters, {});
   });
 }
 
