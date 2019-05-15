0fe9bc0312e503223572ef2d157fbe5d635e802b
sazad44 <530@holbertonschool.com>
2019-05-15 01:55:26 +0000

    Updating task 7 js file to be semistandard compliant

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) console.log(error);
-  characters = JSON.parse(body)['characters']
+  let characters = JSON.parse(body)['characters'];
   characters.map(character => {
     request(character, function (error, response, body) {
       if (error) console.log(error);
