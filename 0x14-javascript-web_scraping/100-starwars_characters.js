7bf21a2d70baf20b8826c1400e9c8d0db825a83c
sazad44 <530@holbertonschool.com>
2019-05-15 01:54:45 +0000

    Uploading task 7 js file to print the names of characters in movies

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  characters = JSON.parse(body)['characters']
+  characters.map(character => {
+    request(character, function (error, response, body) {
+      if (error) console.log(error);
+      console.log(JSON.parse(body)['name']);
+    });
+  });
+});
