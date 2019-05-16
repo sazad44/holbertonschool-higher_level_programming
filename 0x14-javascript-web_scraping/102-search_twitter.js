248a1c8cac20e22551fa0f754fe2286a9e2a07a1
sazad44 <530@holbertonschool.com>
2019-05-16 05:29:05 +0000

    Uploading task 9 js file to send requests to twitter with a search string

---
+++ 0x14-javascript-web_scraping/102-search_twitter.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const base64 = require('base-64');
+const request = require('request');
+const utf8 = require('utf8');
+
+keyCombo = process.argv[2] + ':' + process.argv[3];
+keyComboEncode = encodeURI(keyCombo);
+keyComboEncode64 = base64.encode(keyComboEncode);
+let bearerToken = '';
+let respDict = {};
+let tweetDict = {};
+request.post({url: 'https://api.twitter.com/oauth2/token',
+	      headers: {'Authorization': 'Basic ' + keyComboEncode64,
+		       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+	      body: 'grant_type=client_credentials'
+	     }, (error, response, body) => {
+	       if (error) console.log(error);
+	       respDict = JSON.parse(body);
+	       if (respDict.token_type !== 'bearer') console.log(respDict + 'HEY THIS ISNT THE RIGHT TOKEN');
+	       bearerToken = respDict.access_token;
+	       request({url: 'https://api.twitter.com/1.1/search/tweets.json',
+			headers: {'Authorization': 'Bearer ' + bearerToken},
+			qs: {'q': process.argv[4]}
+		       }, (error, response, body) => {
+			 tweetDict = JSON.parse(body);
+			 for (let i = 0; i < 5; i++) {
+			   status = tweetDict.statuses[i];
+			   output = '[' + status.user.id + '] ' + status.text + ' by ' + status.user.name;
+			   console.log(output);
+			 }
+		       });
+	     });
