fb85bcad34df885faa37f21e027d305215f57a4b
sazad44 <530@holbertonschool.com>
2019-05-17 21:08:24 +0000

    Updating task 6 js file to change header value to correct string"

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,7 +1,7 @@
 $(function () {
   $('DIV#update_header').click(() => {
-    if ($('header').text() !== 'New Header!!') {
-      $('header').text('New Header!!');
+    if ($('header').text() !== 'New Header!!!') {
+      $('header').text('New Header!!!');
     } else {
       $('header').text('First HTML page');
     }
